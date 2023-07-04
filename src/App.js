ebd4bd1d56840cf15347a40298c61daa6eb9ab32
Pavithran2 <pavithrank02@gmail.com>
2023-07-04 21:07:17 +0530

    invoice

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Customer} from './pages/Customer';
 import { Dashboard } from './pages/Dashboard';
 import './App.css';
 import { Analytics } from './pages/Analytics';
+import Invoice from './pages/Invoice';
 
 
 function App() {
@@ -14,10 +15,11 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/dashboard' element={<Dashboard />}/>
-      <Route path='/billing' element={<About />}/>
+      <Route path='/billing' element={<Invoice />}/>
       <Route path='/setting' element={<Setting />}/>
       <Route path='/customer' element={<Customer />}/>
       <Route path='/analytics' element={<Analytics />}/>
+
 
     </Routes>
     
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import { Box, Grid, Typography } from '@mui/material'
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 import SideNav from '../components/SideNav'
 import Header from '../components/Header'
 
 export const Customer = () => {
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <Grid>
       <Header />
+      <Box height={70} />
       <Grid display={'flex'}>
         <SideNav />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Typography variant='h3'>
-            Customer
-          </Typography>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }} >
+        <Grid display={'flex'} flexDirection={'row'}>
+          <FormControl>
+            <FormLabel id="demo-controlled-radio-buttons-group">Customer Type</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              value={value}
+              onChange={handleChange}
+            >
+              <FormControlLabel value="Business" control={<Radio />} label="Business" />
+              <FormControlLabel value="Individual" control={<Radio />} label="Individual" />
+            </RadioGroup>
+          </FormControl>
+          </Grid>
         </Box>
       </Grid>
     </Grid>
