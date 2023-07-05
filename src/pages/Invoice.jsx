10709ed8b0563a6e7d45986e04e516b267bbc9c3
Pavithran2 <pavithrank02@gmail.com>
2023-07-05 11:27:30 +0530

    UI updated

---
+++ src/pages/Invoice.jsx
@@ -0,0 +1,58 @@
+
+import React from 'react'
+import { Box, Grid, Stack, Typography } from '@mui/material'
+import CardContent from '@mui/material/CardContent';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Card from '@mui/material/Card';
+import Tabs from '../components/Tabs';
+import BasicTabs from '../components/Tabs';
+
+const Invoice = () => {
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  
+  };
+  return (
+    <Stack spacing={2} direction={'column'}>
+
+      <Box height={30} />
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card sx={{ height: 60 + 'vh' }}>
+            <CardContent>
+            Customer Type
+            <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={value}
+                onChange={handleChange}
+              >
+                <FormControlLabel value="Business" control={<Radio />} label="Business" />
+                <FormControlLabel value="Individual" control={<Radio />} label="Individual" />
+              </RadioGroup>
+
+            </CardContent>
+
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card sx={{ height: 60 + 'vh' }}>
+            <CardContent>
+              <Typography variant='h6' color={'black'}>
+                Popular Products
+              </Typography>
+              <BasicTabs />
+            </CardContent>
+
+          </Card>
+        </Grid>
+        </Grid>
+    </Stack >
+  )
+}
+
+export default Invoice
