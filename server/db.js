ccb2a11a937908713f59827d72cde0e1063a1a80
Pavithran2 <pavithrank02@gmail.com>
2023-07-11 15:30:04 +0530

    changes in server

--- server/db.js
+++ server/db.js
@@ -1,17 +1,18 @@
 const {MongoClient} = require('mongodb')
 
 let dbConnection
-let url = 'mongodb+srv://pavithran:9790436040@cluster1.oxk2vdh.mongodb.net/'
+let url = 'mongodb://localhost:27017'
 
-module.exports = {
-  connectToDb: (cb) => {
-    MongoClient.connect(url).then((client) => {
-      dbConnection = client.db()
-      return cb()
-    }).catch(err => {
-      console.log(err)
-      return cb(err)
-    })
-  },
-  getDb: () => { dbConnection }
-}
+// module.exports = {
+//   connectToDb: (cb) => {
+//     MongoClient.connect(url).then((client) => {
+//       dbConnection = client.db('mydb')
+//       return cb()
+//     }).catch(err => {
+//       console.log("err",err)
+//       return cb(err)
+//     })
+//   },
+//   getDb: () => { dbConnection }
+// }
+
